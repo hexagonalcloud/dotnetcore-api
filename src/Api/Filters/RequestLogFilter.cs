bc8f7c5c82c59b3a5f23410e99429145d30994a1
Roby Van Damme <roby@robyvandamme.com>
2017-08-10 10:35:57 +0700

    Async RequestLogFilter

--- src/Api/Filters/RequestLogFilter.cs
+++ src/Api/Filters/RequestLogFilter.cs
@@ -1,44 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc.Controllers;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Serilog;
 
 namespace Api.Filters
 {
-	public class RequestLogFilter: IActionFilter
+	public class RequestLogFilter: IAsyncActionFilter
 	{
 		private readonly ILogger _logger;
-		
-		private string _controllerName = string.Empty;
-		private string _actionName = string.Empty;
 			
 		public RequestLogFilter(ILogger logger)
 		{
 			this._logger = logger;
 		}
 		
-		
-		public void OnActionExecuting(ActionExecutingContext context)
+		public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
 		{
+			var controllerName = string.Empty;
+			var actionName = string.Empty;
 			
 			var descriptor = context.ActionDescriptor as ControllerActionDescriptor;
 			if (descriptor != null)
 			{
-				_actionName = descriptor.ActionName;
-				_controllerName = descriptor.ControllerName;
-
+				controllerName = descriptor.ControllerName;
+				actionName = descriptor.ActionName;
 			}			
+			
 			var args = context.ActionArguments;
 
 			// TODO: what else do we need here: application name, machine name? 
-			
-			_logger.Information("Executing Action {ControllerName} - {ActionName} - {@Args} ", _controllerName, _actionName, args)	
-			;
-		}
 
-		public void OnActionExecuted(ActionExecutedContext context)
-		{
-			var args = context.Result;	
-			_logger.Information("Executed Action {ControllerName} - {ActionName} - {@Args} ", _controllerName, _actionName, args);
+			 _logger.Information("Executing Action {ControllerName} - {ActionName} - {@Args} ", controllerName,
+				actionName, args);
+	
+			var actionExecuted = await next(); // the actual action
+
+			var result = actionExecuted.Result;	
+
+			_logger.Information("Executed Action {ControllerName} - {ActionName} - {@Result}", controllerName, actionName, result);		
 		}
 	}
 }
